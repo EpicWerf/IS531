66dd29aea21db182156640917bfa93482339ea93
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-10-06 14:53:18 -0600

    included scripts

--- donutshop/src/server/server.js
+++ donutshop/src/server/server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express")
+const mysql = require("mysql")
 
-const app = express();
-const port = 8000;
-const table ='donut';
+const app = express()
+const port = 8000
+const table = "donut"
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PWD,
-  database: process.env.MYSQL_DB,
-});
+	host: "donutsdb-instance-1.c62rcdsu4itb.us-east-2.rds.amazonaws.com",
+	user: "admin",
+	password: "donuts531",
+	database: "donuts_schema",
+})
 
 app.listen(port, () => {
-  console.log(`App server now listening to port ${port}`);
-});
+	console.log(`App server now listening to port ${port}`)
+})
 
-app.get('/api/donuts', (req, res) => {
-  pool.query(`select * from ${table}`, (err, rows) => {
-    if (err) {
-      res.send(err);
-    } else {
-      console.log(res);
-      res.send(rows);
-    }
-  });
-});
+app.get("/api/donuts", (req, res) => {
+	pool.query(`select * from ${table}`, (err, rows) => {
+		if (err) {
+			res.send(err)
+		} else {
+			console.log(res)
+			res.send(rows)
+		}
+	})
+})
