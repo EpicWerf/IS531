455e4c88f7ac5034dcbd8042280e5c3ba152dbc9
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-10-25 16:53:13 -0600

    Getting powershell set up

---
+++ Powershell/PS Basics.ps1
@@ -0,0 +1,16 @@
+﻿Get-ChildItem
+Get-Process
+Get-Service
+Get-TimeZone
+$a = 1 + 1
+$a
+$a | Get-Member
+$b = 'swag'
+$b
+$b | Get-Member
+Get-ChildItem | Get-Member
+Get-ChildItem
+$a = Get-ChildItem
+$a | Get-Member
+$a.Count
+$a[115]
